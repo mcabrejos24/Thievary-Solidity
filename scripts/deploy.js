2382bcdffc3b01e04397024de3397d6d61c00baf
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-05-04 20:23:34 -0400

    adds deploy.js file

---
+++ scripts/deploy.js
@@ -0,0 +1,44 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('Thief');
+    const gameContract = await gameContractFactory.deploy(
+        ["A", "X", "Y", "Z"],
+        ["gold", "silver", "teal", "purple"],
+        ["https://ipfs.io/ipfs/QmVCEsJhUm2KUxrcHnfEhP45szbnm1nLNiawgXYkT4dx69",
+            "https://ipfs.io/ipfs/QmUbgB3Xm73zCyWycd8eTqRNNtXN53X8jm19vVvovbbXBC",
+            "https://ipfs.io/ipfs/Qmdc1XpWbZCkgi3kzyWnWdUXqQuTVD3fY2y9EcGBJCuLHn",
+            "https://ipfs.io/ipfs/QmdgUCbCJqqebE4Q1FNFTk41etcTiT3zPbcuNN1maqheZn"
+        ]
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+
+    let txn = await gameContract.mintThiefNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #1");
+
+    txn = await gameContract.mintThiefNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #2");
+
+    txn = await gameContract.mintThiefNFT(2);
+    await txn.wait();
+    console.log("Minted NFT #3");
+
+    txn = await gameContract.mintThiefNFT(3);
+    await txn.wait();
+    console.log("Minted NFT #3");
+
+    console.log("Done deploying and minting!");
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
