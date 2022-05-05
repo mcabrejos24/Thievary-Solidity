acf0416206e006cbe77c96f9d592cb21733bb014
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-05-05 01:37:31 -0400

    removes minting from deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,33 +2,15 @@ const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('Thief');
     const gameContract = await gameContractFactory.deploy(
         ["A", "X", "Y", "Z"],
-        ["gold", "silver", "teal", "purple"],
-        ["https://ipfs.io/ipfs/QmVCEsJhUm2KUxrcHnfEhP45szbnm1nLNiawgXYkT4dx69",
-            "https://ipfs.io/ipfs/QmUbgB3Xm73zCyWycd8eTqRNNtXN53X8jm19vVvovbbXBC",
-            "https://ipfs.io/ipfs/Qmdc1XpWbZCkgi3kzyWnWdUXqQuTVD3fY2y9EcGBJCuLHn",
-            "https://ipfs.io/ipfs/QmdgUCbCJqqebE4Q1FNFTk41etcTiT3zPbcuNN1maqheZn"
+        ["Gold", "Silver", "Teal", "Purple"],
+        ["ipfs://QmVCEsJhUm2KUxrcHnfEhP45szbnm1nLNiawgXYkT4dx69",
+            "ipfs://QmUbgB3Xm73zCyWycd8eTqRNNtXN53X8jm19vVvovbbXBC",
+            "ipfs://Qmdc1XpWbZCkgi3kzyWnWdUXqQuTVD3fY2y9EcGBJCuLHn",
+            "ipfs://QmdgUCbCJqqebE4Q1FNFTk41etcTiT3zPbcuNN1maqheZn"
         ]
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
-
-    let txn = await gameContract.mintThiefNFT(0);
-    await txn.wait();
-    console.log("Minted NFT #1");
-
-    txn = await gameContract.mintThiefNFT(1);
-    await txn.wait();
-    console.log("Minted NFT #2");
-
-    txn = await gameContract.mintThiefNFT(2);
-    await txn.wait();
-    console.log("Minted NFT #3");
-
-    txn = await gameContract.mintThiefNFT(3);
-    await txn.wait();
-    console.log("Minted NFT #3");
-
-    console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
